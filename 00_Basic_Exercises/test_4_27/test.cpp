c2cb21b45d64ea6867b9c5f15b1d5c76cab21940
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-27 20:47:19 +0800

    Initialize C++ learning path

---
+++ 00_Basic_Exercises/test_4_27/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "===========================" << std::endl;
+    std::cout << "  恭喜！子文件夹配置大成功！ " << std::endl;
+    std::cout << "===========================" << std::endl;
+    
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    
+    std::cout << "a + b 的值是: " << sum << std::endl;
+    
+    return 0;
+}
